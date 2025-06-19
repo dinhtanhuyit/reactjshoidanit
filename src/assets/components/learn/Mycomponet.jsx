a4c740b269803ed7e5c4ed02b39c2c10cb38a6d6
dinhtanhuyit <dinhtanhuyit@gmail.com>
2025-06-19 15:01:52 +0700

    video 31

--- src/assets/components/learn/Mycomponet.jsx
+++ src/assets/components/learn/Mycomponet.jsx
@@ -2,9 +2,10 @@
  import './style.css';
  // componet = html + css + js
 const MyComponent = () => {
-    
+  const s1 = "hello người đẹp"
   return(
     <>
+    <div>{s1} quá</div>
     <div> huy sos </div>
     <div className="child"></div>
     </>
